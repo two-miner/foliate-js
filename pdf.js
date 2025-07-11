6eddf950e4bbd4c98cdfa221727ddc957b2d90da
limbara <limbaranico@yahoo.com>
2025-07-11 04:54:14 +0000

    fix: avoiding dynamic import.meta.url in pdf.js to avoid vite.dev server problem

--- pdf.js
+++ pdf.js
@@ -1,4 +1,4 @@
-const pdfjsPath = path => new URL(`./vendor/pdfjs/${path}`, import.meta.url).toString()
+const pdfjsPath = path => import.meta.resolve(`./vendor/pdfjs/${path}`)
 
 import './vendor/pdfjs/pdf.mjs'
 const pdfjsLib = globalThis.pdfjsLib
